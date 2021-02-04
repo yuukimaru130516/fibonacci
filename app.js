32aaa3a95f20ba012cb9ae366ab7905a923b1e43
yuukimaru <yuuk130516@gmail.com>
2021-02-04 18:26:28 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function fib(n) {
+    if (n === 0) { 
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    }
+    return fib(n-1) + fib(n-2); // 関数の定義の中で関数自身を呼び出すことを再帰
+    
+}
+
+for (let i = 0; i <= 40; i++) {
+    fib(i);
+    console.log(i + "のフィボナッチ数は" + fib(i) + "です。")
+}
